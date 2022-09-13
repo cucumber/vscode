ef4b6f4671a71ddee319b59965ab66dd4817f2bf
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2022-09-13 07:15:32 +0100

    Fix VscodeFiles

--- src/VscodeFiles.ts
+++ src/VscodeFiles.ts
@@ -1,37 +1,29 @@
 import { Files } from '@cucumber/language-server'
-import { FileSystemProvider, Uri, workspace } from 'vscode'
+import { FileSystem, Uri, workspace } from 'vscode'
 
 export class VscodeFiles implements Files {
-  constructor(private readonly rootUri: string, private readonly provider: FileSystemProvider) {}
+  constructor(private readonly rootUri: string, private readonly fs: FileSystem) {}
 
   async exists(uri: string): Promise<boolean> {
     try {
-      await this.provider.stat(Uri.parse(uri))
+      await this.fs.stat(Uri.parse(uri))
       return true
     } catch {
       return false
     }
   }
 
-  async findFiles(cwd: string, glob: string): Promise<readonly string[]> {
-    const files = await workspace.findFiles(glob)
-    return files.map((file) => file.path)
-  }
-
-  join(...paths: string[]): string {
-    return Uri.joinPath(Uri.parse(this.rootUri), ...paths).path
-  }
-
-  async readFile(path: string): Promise<string> {
-    const data = await this.provider.readFile(Uri.parse(path))
+  async readFile(uri: string): Promise<string> {
+    const data = await this.fs.readFile(Uri.parse(uri))
     return new TextDecoder().decode(data)
   }
 
-  relative(uri: string): string {
-    return uri
+  async findUris(glob: string): Promise<readonly string[]> {
+    const uris = await workspace.findFiles(glob)
+    return uris.map((file) => file.toString())
   }
 
-  toUri(path: string): string {
-    return path
+  relativePath(uri: string): string {
+    return workspace.asRelativePath(Uri.parse(uri), true)
   }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,20 @@
 import { newWasmServer } from '@cucumber/language-server'
 import vscode from 'vscode'
 import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient/node'
+
 import { VscodeFiles } from './VscodeFiles'
 
 let client: LanguageClient
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function activate(context: vscode.ExtensionContext) {
-  // const serverModule = context.asAbsolutePath(path.join('out', 'cucumber-language-server.js'))
-  // const serverOptions: ServerOptions = { module: serverModule, transport: TransportKind.ipc }
-  const makeFiles = (rootUri: string) => new VscodeFiles(rootUri, null)
+  const makeFiles = (rootUri: string) => new VscodeFiles(rootUri, vscode.workspace.fs)
   const serverOptions: ServerOptions = async () => newWasmServer(__dirname, makeFiles)
 
   const clientOptions: LanguageClientOptions = {
     // We need to list all supported languages here so that
     // the language server is notified to reindex when a file changes
+    // https://code.visualstudio.com/docs/languages/identifiers#_known-language-identifiers
     documentSelector: [
       { scheme: 'file', language: 'csharp' },
       { scheme: 'file', language: 'cucumber' },
@@ -23,6 +22,7 @@ export async function activate(context: vscode.ExtensionContext) {
       { scheme: 'file', language: 'php' },
       { scheme: 'file', language: 'ruby' },
       { scheme: 'file', language: 'typescript' },
+      { scheme: 'file', language: 'typescriptreact' },
       { scheme: 'file', language: 'python' },
     ],
   }
