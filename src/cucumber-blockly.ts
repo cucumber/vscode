baaa146adc903e31070da5047e68b019177b3317
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2022-11-18 22:17:45 +0000

    Stronger typing

--- src/cucumber-blockly.ts
+++ src/cucumber-blockly.ts
@@ -1,29 +1,45 @@
 // const vscode = acquireVsCodeApi()
 import { mount } from '@cucumber/blockly'
+import { Suggestion } from '@cucumber/language-server'
 
-import { expressionFromJson, registryFromJson } from './CucumberExpressionsSerde'
+import {
+  expressionFromJson,
+  ExpressionJson,
+  ParameterTypeRegistryJson,
+  registryFromJson,
+} from './CucumberExpressionsSerde'
+
+declare global {
+  interface Window {
+    gherkinSource: string
+    registryJson: ParameterTypeRegistryJson
+    expressionsJson: ExpressionJson[]
+    suggestionsJson: Suggestion[]
+    blocklyMedia: string
+  }
+}
 
 const $app = document.querySelector('#app')
 
-// @ts-ignore
-console.log('registryJson', registryJson)
-// @ts-ignore
-console.log('expressionsJson', expressionsJson)
-
 if ($app) {
-  // @ts-ignore
-  const registry = registryFromJson(registryJson)
-  // @ts-ignore
-  const expressions = expressionsJson.map((expressionJson) =>
+  const registry = registryFromJson(window.registryJson)
+  const expressions = window.expressionsJson.map((expressionJson) =>
     expressionFromJson(registry, expressionJson)
   )
-  // @ts-ignore
-  const suggestions = suggestionsJson
 
-  console.log('TWO')
-
-  // @ts-ignore
-  mount($app, gherkinSource, suggestions, expressions, blocklyMedia, () => {
-    console.log('Blockly changed')
-  })
+  mount(
+    $app,
+    window.gherkinSource,
+    window.suggestionsJson,
+    expressions,
+    window.blocklyMedia,
+    (error, gherkinSource) => {
+      if (error) {
+        console.error(error)
+        return
+      }
+      // TODO: Send a message with the new source
+      console.log(gherkinSource)
+    }
+  )
 }
